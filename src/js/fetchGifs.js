1b3477e61eeada3e5a273f059b140e01d952ca77
Jun1oru <76923595+Jun1oru@users.noreply.github.com>
2024-06-21 23:03:33 +0300

    feat: Cards use giphy api to fetch images.

---
+++ src/js/fetchGifs.js
@@ -0,0 +1,26 @@
+import { config } from './config';
+
+const ApiKey = import.meta.env.VITE_API_KEY || config.API_KEY;
+
+export default async function fetchGif() {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${ApiKey}&q=cat&limit=12&rating=g`,
+      { mode: 'cors' }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    if (data.data.length === 0) {
+      return '';
+    }
+    const gifs = data.data;
+    console.log(gifs);
+    return gifs;
+  } catch (error) {
+    console.error('Error: ', error);
+  }
+}
